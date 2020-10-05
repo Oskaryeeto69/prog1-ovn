69e2167eeeae600a19096cec62e21c3d2634ecef
Oskaryeeto69 <oskar.forsgren@elev.ga.ntig.se>
2020-10-05 12:26:57 +0200

    Update uppgift2.11.py

--- uppgift2.11.py
+++ uppgift2.11.py
@@ -1,11 +1,11 @@
-milnu = input('Vad är mätarställningen idag?')
-milinnan = input('Vad är mätarställningen för ett år sedan?')
-literår = input('Hur många liter har gått i år')
+milnu = input('Vad är mätarställningen idag?') #ställer en fråga
+milinnan = input('Vad är mätarställningen för ett år sedan?') #ställer en fråga
+literår = input('Hur många liter har gått i år') #ställer en fråga
 
-milnu = int(milnu)
-milinnan = int(milinnan)
-körtdettaår = milnu-milinnan
-literår = float(literår)
+milnu = int(milnu) #säger att milen som är på mätaren nu är en int
+milinnan = int(milinnan) #säger att milen som var på mätaren ett år sen är en int
+körtdettaår = milnu-milinnan #räknar hur många mil bilen har kört detta år
+literår = float(literår) #säger att liter
 literpermil = literår/körtdettaår
 
 print(f'Så här många mil har du kört i år:{körtdettaår:.0f}')
