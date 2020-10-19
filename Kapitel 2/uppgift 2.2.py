7ab27596f43bd1921f14256429fadf539a2868f4
Oskaryeeto69 <oskar.forsgren@elev.ga.ntig.se>
2020-10-19 14:51:27 +0200

    yeet

--- Kapitel 2/uppgift 2.2.py
+++ Kapitel 2/uppgift 2.2.py
@@ -1,6 +1,6 @@
-svar = input('skriv ett tal: ')
-x=float(svar)
-y=x * x
-a=x * x * x * x
-print('Talet i kvadrat är', y)
-print('Talet upphöjt i 4 är', a)
+svar = input('skriv ett tal: ') # Säger att vi ska skriva in ett tal
+x=float(svar) # Säger att x är en float
+y=x * x # Räknar ut vad talet i kvadrat är
+a=x * x * x * x # Räknar ut vad talet upphöjt i 4 är
+print('Talet i kvadrat är', y) # Printar ut svaret för talet i kvadrat
+print('Talet upphöjt i 4 är', a) # Printar ut svaret för talet upphöjt i 4
--- Kapitel 2/uppgift 2.3.py
+++ Kapitel 2/uppgift 2.3.py
@@ -1,4 +1,4 @@
-svar = input('skriv ett tal: ')
-x=float(svar)
-y=x * x
-print(f'Talet i kvadrat är {y:6.2f}')
+svar = input('skriv ett tal: ') # Säger att vi ska skriva in ett tal
+x=float(svar) # Säger att x är en float
+y=x * x # Räknar ut vad talet är i kvadrat
+print(f'Talet i kvadrat är {y:6.2f}') # Gör så att svaret blir i decimaler
